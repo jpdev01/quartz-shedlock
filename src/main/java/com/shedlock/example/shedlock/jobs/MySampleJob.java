21cee060068ea2b982977542f3918cabf2513360
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2025-02-17 12:38:24 -0300

    chore: improving code

--- src/main/java/com/shedlock/example/shedlock/jobs/MySampleJob.java
+++ src/main/java/com/shedlock/example/shedlock/jobs/MySampleJob.java
@@ -3,32 +3,18 @@ package com.shedlock.example.shedlock.jobs;
 import net.javacrumbs.shedlock.core.LockAssert;
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.quartz.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.quartz.QuartzJobBean;
-import org.springframework.stereotype.Component;
-
-import static org.quartz.JobBuilder.newJob;
-import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
-import static org.quartz.TriggerBuilder.newTrigger;
 
 public class MySampleJob extends QuartzJobBean {
 
-	private String name;
-
-    public MySampleJob()  {
+    public MySampleJob() {
     }
 
-	// Inject the "name" job data property
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	@Override
+    @Override
     @SchedulerLock(name = "my_sample_job_lock", lockAtLeastFor = "PT5S", lockAtMostFor = "PT30S")
     protected void executeInternal(JobExecutionContext context) {
-        LockAssert.assertLocked(); // Garante que o lock foi adquirido
-
+        LockAssert.assertLocked();
         System.out.println("Hello World!");
-	}
+    }
 
 }
